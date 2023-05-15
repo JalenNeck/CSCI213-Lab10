088c64ebaba2fd3ceb05e44bbe12cac53f1215cb
Jalen D. Neck <JalenNeck@go.rmc.edu>
2023-05-15 14:26:35 -0400

    updated README.md

---
+++ git add Car.java
@@ -0,0 +1,29 @@
+[33mcommit 2ce2a369bf708379e1fc4644b59a138a36bb84ee[m[33m ([m[1;36mHEAD -> [m[1;32mNeck[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mmyNewBranch[m[33m, [m[1;32mmain[m[33m)[m
+Author: Robin M. Givens <69699276+rmgivens@users.noreply.github.com>
+Date:   Mon May 3 15:38:03 2021 -0400
+
+    Update .gitignore
+
+[33mcommit 15514a9a249255a2cef896a452b8371b5d6a2df7[m
+Author: Robin M. Givens <69699276+rmgivens@users.noreply.github.com>
+Date:   Sun May 2 14:05:37 2021 -0400
+
+    Create .gitignore
+
+[33mcommit fbf6921f4ea0d70de940f1fc0daa839bb4c63c18[m
+Author: Robin M. Givens <69699276+rmgivens@users.noreply.github.com>
+Date:   Sun May 2 11:48:27 2021 -0400
+
+    Update README.md
+
+[33mcommit 7336f3ae41068ca989dd284a1f62be0c14430eb0[m
+Author: Robin M. Givens <69699276+rmgivens@users.noreply.github.com>
+Date:   Sun May 2 11:45:52 2021 -0400
+
+    Add Vehicle.java
+
+[33mcommit eb94d81609243e18bcd41e76d50d0a1a7a5b2594[m
+Author: Robin M. Givens <69699276+rmgivens@users.noreply.github.com>
+Date:   Sun May 2 11:44:29 2021 -0400
+
+    Initial commit
